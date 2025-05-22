58242d63a3ae626949fdd42b832f984d1a6b958d
rubensr <rsepulvedareal@gmail.com>
2025-05-22 15:40:14 +0100

    Update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,11 +1,11 @@
 <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
 <div id="footer">
     <img id="logo" src="/images/logoEML.png" alt="Not found">
-    <p>contact@company.com</p>
-    <p>+123 456 7890</p>
-    <p>&copy; 2024 Company Name. All Rights Reserved.</p>
+    <p>enjoyhomelanzarote@gmail.com</p>
+    <p>+34 XXXXXXXX</p>
+    <p>&copy; 2025 Enjoy Home Lanzarote. All Rights Reserved.</p>
     @if(session('is_admin'))
-        <a id="a-footer" href="{{ route('admin.logout') }}">Log out</a> 
+        <a id="a-footer" href="{{ route('admin.logout') }}">Log out</a>
     @else
         
     @endif
