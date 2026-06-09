43652632f63eb25b31253ab09dfe23941b7c0377
rubensr <rsepulvedareal@gmail.com>
2026-06-09 12:59:45 +0100

    add form requests for super admin create and update

---
+++ app/Http/Requests/StoreSuperAdminRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSuperAdminRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name'         => ['required', 'string', 'max:255'],
+            'email'        => ['required', 'email', 'max:255', 'unique:users,email'],
+            'phone_number' => ['nullable', 'string', 'max:20'],
+            'password'     => [
+                'required',
+                'string',
+                'min:8',
+                'confirmed',
+                'regex:/[a-z]/',
+                'regex:/[A-Z]/',
+                'regex:/[0-9]/',
+            ],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.unique'       => 'This email is already registered.',
+            'password.min'       => 'Password must be at least 8 characters.',
+            'password.confirmed' => 'Passwords do not match.',
+            'password.regex'     => 'Password must contain uppercase, lowercase and numbers.',
+        ];
+    }
+}
