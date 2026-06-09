eb8c219e0a76ad40443c6f802b2ce7cfa0052dfa
rubensr <rsepulvedareal@gmail.com>
2026-06-09 13:00:33 +0100

    add super_admin route group under /super-admin prefix

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\MailController;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\ReservationPriceController;
+use App\Http\Controllers\SuperAdminController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\IsAdmin;
 use App\Models\Property;
@@ -25,46 +26,39 @@ Route::post('/send-email', [MailController::class, 'sendEmail'])->name('send.ema
 
 Route::get('/property/{id}/reservations', [ReservationController::class, 'data'])->name('property.reservations.data');
 
+// --- Admin routes --- 
 Route::middleware([IsAdmin::class])->group(function (): void {
     Route::post('/admin/logout', [AdminController::class, 'logoutFunction'])->name('admin.logout');
-
     Route::get('/admin/properties', [AdminController::class, 'properties'])->name('admin.properties');
-
     Route::get('/admin/reservations/pending', [AdminController::class, 'pending'])->name('admin.reservations.pending');
-
     Route::post('/admin/reservations/pending/update/{id}', [AdminController::class, 'updateStatus'])->name('admin.reservations.pending.update');
-
     Route::put('/admin/properties/update/{property}', [AdminController::class, 'updateProperty'])->name('admin.properties.update');
-
     Route::delete('/properties/{id}', [PropertyController::class, 'destroy'])->name('properties.destroy');
-
     Route::get('/properties/create', [PropertyController::class, 'create'])->name('properties.create');
-
     Route::post('/properties/store', [PropertyController::class, 'store'])->name('properties.store');
-
     Route::get('/properties/{id}/edit', [PropertyController::class, 'edit'])->name('properties.edit');
-
     Route::put('/properties/{property}', [PropertyController::class, 'update'])->name('properties.update');
-
     Route::get('/suggestion/{reservation}', [AdminController::class, 'suggestionEmail'])->name('suggestion.create');
-
     Route::post('/reservations/{id}/send-suggestion', [MailController::class, 'sendSuggestion'])->name('reservations.sendSuggestion');
-
     Route::get('/admin/calendar', [AdminController::class, 'calendar'])->name('admin.calendar');
-
     Route::get('/admin/calendar/reservations', [AdminController::class, 'getConfirmedReservations'])->name('admin.calendar.reservations');
-
     Route::post('/admin/calendar/reservation/update-time', [AdminController::class, 'updateTime'])->name('admin.calendar.reservations.update-time');
-
     Route::get('/admin/calendar/export-excel', [AdminController::class, 'exportExcel'])->name('admin.calendar.export-excel');
-
     Route::get('/admin/calendar/export-factura-excel', [AdminController::class, 'exportfacturaExcel'])->name('admin.calendar.export-factura-excel');
-
     Route::get('/admin/reservation-prices', [ReservationPriceController::class, 'index'])->name('admin.reservation_prices');
-
     Route::delete('/reservation-prices/{id}', [ReservationPriceController::class, 'destroy'])->name('reservation-prices.destroy');
-
     Route::post('/reservation-prices/create', [ReservationPriceController::class, 'create'])->name('reservation-prices.create');
+});
+
+// --- Super Admin routes ---
+Route::middleware(['super_admin'])->prefix('super-admin')->name('super_admin.')->group(function (): void {
+    Route::get('/admins', [SuperAdminController::class, 'index'])->name('index');
+    Route::get('/admins/create', [SuperAdminController::class, 'create'])->name('create');
+    Route::post('/admins', [SuperAdminController::class, 'store'])->name('store');
+    Route::get('/admins/{admin}/edit', [SuperAdminController::class, 'edit'])->name('edit');
+    Route::put('/admins/{admin}', [SuperAdminController::class, 'update'])->name('update');
+    Route::post('/admins/{admin}/toggle', [SuperAdminController::class, 'toggleAdmin'])->name('toggle');
+    Route::delete('/admins/{admin}', [SuperAdminController::class, 'destroy'])->name('destroy');
 });
 
 Route::get('/api/property-price-range', [ReservationPriceController::class, 'getPriceRange']);
@@ -74,7 +68,6 @@ Route::get('/privacy', function () {
 })->name('privacy');
 
 Route::get('/api/images', function () {
-    // Assuming you store the featured image per property
     $properties = Property::all();
 
     $images = $properties->mapWithKeys(function ($property) {
