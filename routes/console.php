67cde2039d7ddf66986d4b0313e9b0219b7e3aea
rubensr <rsepulvedareal@gmail.com>
2026-06-30 18:43:25 +0100

    remove duplicated scheduled command registration

--- routes/console.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Schedule;
-
-Schedule::command('app:send-reservation-info')
-    ->dailyAt('09:00');
-
-//  When id do the development * * * * * cd /var/www/bookingWeb && php artisan schedule:run >> /dev/null 2>&1
