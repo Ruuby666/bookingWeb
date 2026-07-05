f8e216477df705587a02c35807c8aceb13399114
rubensr <rsepulvedareal@gmail.com>
2026-07-05 03:12:47 +0100

    fix the buttons for the properties to move

--- public/js/index.js
+++ public/js/index.js
@@ -153,14 +153,14 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
-    prevButton.addEventListener("gmp-click", function () {
+    prevButton.addEventListener("click", function () {
         carousel.scrollBy({
             left: -cardWidth,
             behavior: "smooth"
         });
     });
 
-    nextButton.addEventListener("gmp-click", function () {
+    nextButton.addEventListener("click", function () {
         carousel.scrollBy({
             left: cardWidth,
             behavior: "smooth"
