35350f12ea45fa6cc10229388221f6723210b53a
rubensr <rsepulvedareal@gmail.com>
2026-07-06 20:13:09 +0100

    remove unused UserService, UpdateUserRequest and UserPolicy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,11 +5,9 @@ namespace App\Providers;
 use App\Models\Property;
 use App\Models\Reservation;
 use App\Models\ReservationPrice;
-use App\Models\User;
 use App\Policies\PropertyPolicy;
 use App\Policies\ReservationPolicy;
 use App\Policies\ReservationPricePolicy;
-use App\Policies\UserPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,6 +23,5 @@ class AppServiceProvider extends ServiceProvider
         Gate::policy(Property::class, PropertyPolicy::class);
         Gate::policy(Reservation::class, ReservationPolicy::class);
         Gate::policy(ReservationPrice::class, ReservationPricePolicy::class);
-        Gate::policy(User::class, UserPolicy::class);
     }
 }
