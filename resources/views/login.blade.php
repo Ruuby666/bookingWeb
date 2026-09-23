925164f0fc453f50952aba7fa037faa8a54bbe89
rubensr <rsepulvedareal@gmail.com>
2026-09-23 06:01:56 +0100

    add missing charset and viewport meta tags to home and login pages

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log In</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
